aedc9cd9c9ab3ff3a5bc4b51b6471c3a6cccfd32
amasroeda <amasroeda@gmail.com>
2023-12-18 09:23:57 +0000

    modifications

--- models/Patients.js
+++ models/Patients.js
@@ -14,21 +14,16 @@ const emergencyConstant = mongoose.Schema(
         relationshipWithPatient: {type: String}
 }
 )
-const Patients = mongoose.Schema(
-    {
-        PatientID:{type: String,required : [true, "Please enter Patient's name"]},
-        Surname:{type: String},
-        OtherName:{type: String},
-        Gender: {type: String},
-        PhoneNumber: {type: String},
-        ResidentialAddress : {type: String}, 
-        EmergencyContact : {EmergencyContact}, 
-        Contact: {type: String},
-        relationshipWithPatient: {type: String}
-    },
-    {
-        timestamps: true 
-    }
-)
+const Patients = new Schema({
+        PatientID: String,
+        Surname: String,
+        OtherName: String,
+        Gender: String,
+        PhoneNumber: String,
+        ResidentialAddress : String, 
+        EmergencyContact : EmergencyContact, 
+        Contact:String,
+        relationshipWithPatient: String
+    },)
 module.EXPORTS = Patients;
 const Product = mongoose.model('Product', productschema);
